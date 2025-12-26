f3bfbf572d456ea3d1b8970568e96e99ddec47ca
annashelyakina <anna.shelyakina82@gmail.com>
2025-12-26 22:08:12 +0300

    updated TestBase

--- src/test/java/ru/arcadia/TestBase.java
+++ src/test/java/ru/arcadia/TestBase.java
@@ -20,7 +20,7 @@ public class TestBase {
         Configuration.browserVersion = System.getProperty("browserVersion", "128.0");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.timeout = 10000;
-        Configuration.remote = ("https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.remote = System.getProperty("remote");
 
        // Настраиваем capabilities для Selenoid
         DesiredCapabilities capabilities = new DesiredCapabilities();
